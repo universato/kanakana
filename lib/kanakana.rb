90094ef9ff4c1aaca1678bb0b9bc1b272f498104
universato <universato@gmail.com>
2020-11-25 10:43:21 +0900

    20201125

--- lib/kanakana.rb
+++ lib/kanakana.rb
@@ -1,6 +1,138 @@
-require "kanakana/version"
+# frozen_string_literal: true
 
+require 'kanakana/version'
+require 'nkf'
+
+# Kanakana
 module Kanakana
-  class Error < StandardError; end
-  # Your code goes here...
+  DAKUON_HIRAGANA = {
+    'か' => 'が', 'き' => 'ぎ', 'く' => 'ぐ', 'け' => 'げ', 'こ' => 'ご',
+    'さ' => 'ざ', 'し' => 'じ', 'す' => 'ず', 'せ' => 'ぜ', 'そ' => 'ぞ',
+    'た' => 'だ', 'ち' => 'ぢ', 'つ' => 'づ', 'て' => 'で', 'と' => 'ど',
+    'は' => 'ば', 'ひ' => 'び', 'ふ' => 'ぶ', 'へ' => 'べ', 'ほ' => 'ぼ',
+    'う' => 'ゔ', 'ゝ' => 'ゞ'
+  }.freeze
+  DAKUTENABLE_HIRAGANA = DAKUON_HIRAGANA.keys
+
+  DAKUON_KATAKANA = {
+    'カ' => 'ガ', 'キ' => 'ギ', 'ク' => 'グ', 'ケ' => 'ゲ', 'コ' => 'ゴ',
+    'サ' => 'ザ', 'シ' => 'ジ', 'ス' => 'ズ', 'セ' => 'ゼ', 'ソ' => 'ゾ',
+    'タ' => 'ダ', 'チ' => 'ヂ', 'ツ' => 'ヅ', 'テ' => 'デ', 'ト' => 'ド',
+    'ハ' => 'バ', 'ヒ' => 'ビ', 'フ' => 'ブ', 'ヘ' => 'ベ', 'ホ' => 'ボ',
+    'ウ' => 'ヴ', 'ヽ' => 'ヾ',
+    'ワ' => 'ヷ', 'ヰ' => 'ヸ', 'ヱ' => 'ヹ', 'ヲ' => 'ヺ'
+  }.freeze
+  DAKUTENABLE_KATAKANA = DAKUON_KATAKANA.keys
+
+  DAKUON_TABLE = DAKUON_HIRAGANA.merge(DAKUON_KATAKANA)
+  DAKUTENABLES = DAKUTENABLE_HIRAGANA + DAKUTENABLE_KATAKANA
+
+  HANDAKUON_TABLE = {
+    'は' => 'ぱ', 'ひ' => 'ぴ', 'ふ' => 'ぷ', 'へ' => 'ぺ', 'ほ' => 'ぽ',
+    'ハ' => 'パ', 'ヒ' => 'ピ', 'フ' => 'プ', 'ヘ' => 'ペ', 'ホ' => 'ポ'
+  }.freeze
+
+  KOGAKI_HIRAGANA = {
+    'ぁ' => 'あ', 'ぃ' => 'い', 'ぅ' => 'う', 'ぇ' => 'え', 'ぉ' => 'お',
+    'ゕ' => 'か', 'ゖ' => 'け',
+    'っ' => 'つ',
+    'ゃ' => 'や', 'ゅ' => 'ゆ', 'ょ' => 'よ',
+    'ゎ' => 'わ'
+  }.freeze
+  KOGAKI_KATAKANA = {
+    'ァ' => 'ア', 'ィ' => 'イ', 'ゥ' => 'ウ', 'ェ' => 'エ', 'ォ' => 'オ',
+    'ヵ' => 'カ', 'ヶ' => 'ケ',
+    'ッ' => 'ツ',
+    'ャ' => 'ヤ', 'ュ' => 'ユ', 'ョ' => 'ヨ',
+    'ヮ' => 'ワ'
+  }.freeze
+
+  HIRAGANA_SMALL_LIST = KOGAKI_HIRAGANA.keys
+
+  ITAIJI = {
+    'ゐ' => 'い', 'ゑ' => 'え',
+    'ヰ' => 'イ', 'ヱ' => 'エ', 'ヲ' => 'オ'
+  }.freeze
+
+  # p ("ｱ".."ﾜ").map{ |c| NKF.nkf("-Ww --hiragana", c) }.push("を", "ん")
+  # 五十音
+  HIRAGANA = %w[
+    あ い う え お
+    か き く け こ
+    さ し す せ そ
+    た ち つ て と
+    な に ぬ ね の
+    は ひ ふ へ ほ
+    ま み む め も
+    や ゆ よ
+    ら り る れ ろ
+    わ を ん
+  ].freeze
+
+  KATAKANA = %w[
+    ア イ ウ エ オ
+    カ キ ク ケ コ
+    サ シ ス セ ソ
+    タ チ ツ テ ト
+    ナ ニ ヌ ネ ノ
+    ハ ヒ フ ヘ ホ
+    マ ミ ム メ モ
+    ヤ ユ ヨ
+    ラ リ ル レ ロ
+    ワ ヲ ン
+  ].freeze
+
+  NOT_DAKUTENABLE_HIRAGANA = HIRAGANA - DAKUON_HIRAGANA.keys
+  NOT_DAKUTENABLE_KATAKANA = KATAKANA - DAKUON_KATAKANA.keys
+  NOT_DAKUTENABLES = NOT_DAKUTENABLE_HIRAGANA + NOT_DAKUTENABLE_KATAKANA
+
+  class << self
+    def nigorize(str)
+      pattern = /[#{DAKUTENABLES.join}]/
+      str.unicode_normalize.gsub(pattern, DAKUON_TABLE)
+    end
+
+    def hyper_nigorize(str)
+      pattern = /([#{NOT_DAKUTENABLES.join}])/
+      nigorize(str).gsub(pattern) { "#{Regexp.last_match(1)}\u3099" }
+    end
+
+    def hiraganize(str)
+      NKF.nkf('-w --hiragana', str)
+    end
+
+    def katakanize(str)
+      NKF.nkf('-w --katakana', str)
+    end
+
+    def make_ascii(str)
+      # NKF.nkf('-m0Z1 -w', str)
+      NKF.nkf('-m0Z1 -W -w', str)
+      NKF.nkf('-Z1 -Ww', str)
+      # str.tr("０-９ａ-ｚＡ-Ｚ．，＠−", "0-9a-zA-Z.,@-")
+      # str.tr('！-～', '!-~')
+      # str.tr('　-～', ' -~')
+    end
+
+    def unicodes(str)
+      # str.unpack('U*').map { |c| "U+#{c.to_s(16).upcase}" }
+      str.codepoints.map { |c| "U+#{c.to_s(16).upcase}" }
+    end
+
+    def escape(str)
+      # str.unpack('U*').map { |c| "\\u#{c.to_s(16).upcase}" }.join
+      str.codepoints.map { |c| "\\u#{c.to_s(16).upcase}" }.join
+    end
+
+    def ords
+      str.chars.zip(str.codepoints.map(&:ord)).to_h
+    end
+
+    # charset
+    def table(str)
+      str.chars.zip(str.codepoints.map { |c| "\\u#{c.to_s(16).upcase}" }).to_h
+    end
+  end
 end
+
+KanaKana = Kanakana
--- spec/kanakana_spec.rb
+++ spec/kanakana_spec.rb
@@ -1,9 +1,136 @@
 RSpec.describe Kanakana do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Kanakana::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'test nigorize' do
+    src = []
+    dst = []
+    src << 'にほんごカタカナ生きたい'
+    dst << 'にぼんごガダガナ生ぎだい'
+
+    # 結合文字はは正規化する。
+    src << 'が' # "\u304B\u3099" # 結合文字
+    dst << 'が' # "\u304C"
+
+    src << "\u304B\u3099"
+    dst << "\u304C"
+    src.zip(dst) do |src, dst|
+      expect(Kanakana.nigorize(src)).to eq(dst)
+    end
+  end
+
+  it 'test hyper_nigorize' do
+    src = []
+    dst = []
+    src << 'にほんごカタカナ生きたい'
+    dst << 'に゙ぼん゙ごガダガナ゙生ぎだい゙'
+
+    # 結合文字はは正規化する。
+    src << 'が' # "\u304B\u3099" # 結合文字
+    dst << 'が' # "\u304C"
+
+    src << "\u304B\u3099"
+    dst << "\u304C"
+    src.zip(dst) do |src, dst|
+      expect(Kanakana.hyper_nigorize(src)).to eq(dst)
+    end
+  end
+
+  it 'test katakanize' do
+    src = ['']
+    dst = src.map(&:dup)
+
+    src << 'ゐゑにほんごニホンゴﾆﾎﾝｺﾞ'
+    dst << 'ヰヱニホンゴニホンゴニホンゴ'
+    src.zip(dst) do |src, dst|
+      expect(Kanakana.katakanize(src)).to eq(dst)
+    end
+  end
+
+  it 'test hiraganize' do
+    src = ['']
+    dst = src.map(&:dup)
+
+    src << 'ｺﾅｺﾞﾅ、ﾊﾝｶｸ, ゼンカク。半角スペース→ |　←全角スペース。ひらがなヰヱゟ'
+    dst << 'こなごな、はんかく, ぜんかく。半角すぺーす→ |　←全角すぺーす。ひらがなゐゑゟ'
+
+    src << "\uFF9E\uFF9F" # 半角カナの濁点,半濁点
+    dst << "\u309B\u309C" # 全角仮名の濁点、半濁点 (not 結合文字)
+    src.zip(dst) do |src, dst|
+      expect(Kanakana.hiraganize(src)).to eq(dst)
+    end
+  end
+
+  # [Rubyで文字化けせずに全角英数字を半角英数字に変換する方法 \- Qiita](https://qiita.com/y-ken/items/17181f322d0413edd3dc)
+  it 'test make_ascii' do
+    src = ['ルⅡ', 'Ruby Ⅱ', "\u30FC"]
+    dst = src.map(&:dup)
+    # "\u30FC"は、長音(全角の長音、カタカナの後ろにあるのでカタカナと同類だろう)
+
+    src << 'Ｒｕｂｙ-２．２'
+    dst << 'Ruby-2.2'
+
+    src << "\uFF0D" # 全角ハイフン(マイナスの意味ももつ) # 全角アスキーの並びにある
+    dst << '-'
+
+    # src << '‐' # "\u2010" # ハイフン。MacでGoogle日本語入力だと「全角ハイフン」というが、全角に見えない。
+    # dst << '-' # "\u002D" # アスキーの半角ハイフン=マイナス
+
+    src << '−' # "\u2212" # 全角のマイナス
+    dst << '-' # "\u002D" # 半角ハイフン(マイナスの意味も持つ)
+
+    src << 'ｒｕｂｙ−ｌａｎｇ＠ｅｘａｍｐｌｅ．ｃｏｍ'
+    dst << 'ruby-lang@example.com'
+
+    src << '全角カンマ，は半角カンマに'
+    dst << '全角カンマ,は半角カンマに'
+
+    src << '！？'
+    dst << '!?'
+
+    src.zip(dst) do |src, dst|
+      # p Kanakana.unicodes(src)
+      # p Kanakana.unicodes(dst)
+      expect(Kanakana.make_ascii(src)).to eq(dst)
+    end
+  end
+
+  it 'test unicodes' do
+    expect(Kanakana.unicodes('𩸽')).to eq(['U+29E3D'])
+    src = ['']
+    dst = [[]]
+
+    src << '👨‍👩‍👧‍👦'
+    dst << ['U+1F468', 'U+200D', 'U+1F469', 'U+200D', 'U+1F467', 'U+200D', 'U+1F466']
+    src.zip(dst) do |src, dst|
+      expect(Kanakana.unicodes(src)).to eq(dst)
+    end
+  end
+
+  it 'escape' do
+    expect(Kanakana.escape('𩸽')).to eq('\u29E3D')
+    src = ['']
+    dst = src.map(&:dup)
+
+    src << '👨‍👩‍👧‍👦'
+    dst << '\u1F468\u200D\u1F469\u200D\u1F467\u200D\u1F466'
+    src.zip(dst) do |src, dst|
+      expect(Kanakana.escape(src)).to eq(dst)
+    end
+  end
+
+  it 'table' do
+    src = ['']
+    dst = [{}]
+
+    src << 'たちまち'
+    dst << { 'た' => '\u305F', 'ち' => '\u3061', 'ま' => '\u307E' }
+
+    src << '👨‍👩‍👧‍👦'
+    dst << { '👨' => '\u1F468', '‍' => '\u200D', '👩' => '\u1F469', '👧' => '\u1F467', '👦' => '\u1F466' }
+    src.zip(dst) do |src, dst|
+      expect(Kanakana.table(src)).to eq(dst)
+    end
   end
 end
